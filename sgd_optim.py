6d9e146bff6c4e796b14ee274bd2147427804530
Shashank M <shashank.1842000@gmail.com>
2020-11-28 11:45:16 +0530

    modified sgd

--- sgd_optim.py
+++ sgd_optim.py
@@ -9,18 +9,18 @@ loss_function=ob.objective
 no_iters=600
 torch.manual_seed(2)
 
-power_init=torch.abs(torch.randn(10))+8
+power_init=torch.abs(torch.randn(10))+8 # initial guess of power
 
 power_init.requires_grad=True
 
 for i in range(no_iters):
     loss=ob.objective(power_init,False) # here loss is max(-secrecy rate) which we are minimising.
-    loss.backward(retain_graph=True)
+    loss.backward(retain_graph=True) # find gradient of loss wrt power.
 
     power_init.data -= power_init.grad*50 # update power in direction of negative gradient.
     power_init.grad.zero_()
 
-    power_init.data=torch.clamp(power_init.data,min=0,max=10)
+    power_init.data=torch.clamp(power_init.data,min=0,max=10) # putting bound on power b/w 0 and 10.
     
 
     if i%100==0:
